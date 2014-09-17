6996a671bae801cd55eb3367b7708c81fc7dd849
phriscage <phriscage@gmail.com>
2014-09-17 02:34:12 +0000

    adding the settings menu

--- www/core/views.py
+++ www/core/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, request, redirect, url_for, render_template, g, \
+    jsonify, session
 from flask.ext.login import login_required
 
 core = Blueprint('core', __name__, template_folder='templates')
@@ -6,6 +7,8 @@ core = Blueprint('core', __name__, template_folder='templates')
 @core.route('/', methods=['GET'])
 def get_index():
     """ root path render main """
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('core.get_scores'))
     first_name = request.args.get('f')
     last_name = request.args.get('l')
     email_address = request.args.get('e')
@@ -17,3 +20,9 @@ def get_index():
 def get_test():
     """ root path render main """
     return render_template('core/test.html')
+
+@core.route('/scores', methods=['GET'])
+@login_required
+def get_scores():
+    """ scores render top scores """
+    return render_template('core/scores.html')
